145f0861bffb3c8bd47cf6449e1cc3289ecefc31
최성준 <db970406@naver.com>
2022-02-23 21:22:41 +0900

    feat: Add NavBar 로그인 호버이벤트 추가
    1. 무엇을? : NavBar 부분 로그인 호버 시 소셜로그인 리스트 보여주게 설정함
    2. 왜? : 소셜로그인은 페이지 전환 없이도 가능케 하기 위함

--- components/post/search/SearchInputBtn.tsx
+++ components/post/search/SearchInputBtn.tsx
@@ -1,6 +1,6 @@
 /**
  * 생성일: 2022.02.21
- * 수정일: ------
+ * 수정일: 2022.02.23
  */
 
 import { motion } from 'framer-motion';
@@ -26,14 +26,21 @@ export default function SearchInputBtn() {
         isSearchMode ? (
             <SearchInput setIsSearchMode={setIsSearchMode} />
         ) : (
+
             <motion.button
                 onClick={() => setIsSearchMode(true)}
                 variants={offVar}
                 initial="invisible"
                 animate="visible"
+                whileHover={{
+                    scale: 1.2,
+                    transition: {
+                        duration: 0.3
+                    }
+                }}
             >
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="white">
-                    <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="white">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
             </motion.button>
         )
