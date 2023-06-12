7b0932cb4267b1b53b372a91a48dac665a003011
dv970406 <dv970406@gmail.com>
2023-06-13 00:48:57 +0900

    feat: Modify sociallogin disabled

--- components/user/create/SocialLogin.tsx
+++ components/user/create/SocialLogin.tsx
@@ -14,12 +14,12 @@ interface ISocialLogin {
 
 const SocialLogin = ({ social, isAuthPage = false }: ISocialLogin) => {
   return (
-    /* 네이버는 유효기간 만료로 다시 인증해야되는데 과정이 까다로워서 일단 막아놓기 */
+    /* 소셜로그인은 잘되다가 너무 안써서 각 플랫폼에서 막아버림. 네이버는 유효기간 만료로 다시 인증해야되는데 과정이 까다로워서 일단 막아놓기 */
     <button
-      disabled={social === "naver"}
+      disabled={true}
       style={{
-        opacity: social === "naver" ? 0.05 : 1,
-        pointerEvents: social === "naver" ? "none" : "auto",
+        opacity: true ? 0.05 : 1,
+        pointerEvents: true ? "none" : "auto",
       }}
     >
       <Link href={makeSocialLoginReqUrl({ socialSite: social })} passHref>
